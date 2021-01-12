0f4efea185a2ec2d28731eeef24b386c1e2b31f4
augustustse <77156813+augustustse@users.noreply.github.com>
2021-01-12 11:20:46 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
+let number = 0
 input.onButtonPressed(Button.A, function () {
-    let temperature = 0
-    basic.showString("" + (temperature))
+    basic.showString("" + (input.temperature()))
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(input.lightLevel())
 })
 input.onGesture(Gesture.Shake, function () {
     number += 1
-})
-let number = 0
-basic.forever(function () {
-    basic.showNumber(number)
 })
